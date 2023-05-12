81c84073045dcc8310977c25781583db477861af
unknown <you@example.com>
2023-05-12 22:03:49 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,137 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room-4k-1",
+      "name": "LIVING ROOM 4K 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.24423808719701334,
+        "pitch": 0.047155309648617916,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8792838335199491,
+          "pitch": 0.09291788181609917,
+          "rotation": 0,
+          "target": "1-living-room--4k-2"
+        },
+        {
+          "yaw": -0.3384054256916098,
+          "pitch": 0.10543094080128057,
+          "rotation": 0,
+          "target": "2-4k-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-room--4k-2",
+      "name": "LIVING ROOM  4K 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.030834717555284996,
+        "pitch": 0.12520581951864962,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2886447764608455,
+          "pitch": 0.10594070559982427,
+          "rotation": 0,
+          "target": "0-living-room-4k-1"
+        },
+        {
+          "yaw": -0.5607690253414876,
+          "pitch": 0.1273947250967158,
+          "rotation": 0,
+          "target": "2-4k-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-4k-bedroom",
+      "name": "4K BEDROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.180836811083461,
+        "pitch": 0.23504839758068385,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.783919701858931,
+          "pitch": 0.11083437343811298,
+          "rotation": 0,
+          "target": "1-living-room--4k-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SECOND FLOOR ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
